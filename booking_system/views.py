ee3df75c7845900a97e9b3a44293415cf628714b
Marcel <marceli.lagoda@gmail.com>
2020-02-15 22:16:56 +0100

    Import required  modules into views.py

--- booking_system/views.py
+++ booking_system/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from dateutil import parser
+from django.core.exceptions import ObjectDoesNotExist
+from django.shortcuts import render, redirect, get_object_or_404
+from django.views import View
+import datetime
+from .forms import CreateRoomForm
+from .models import Room, Booking
 
 # Create your views here.
