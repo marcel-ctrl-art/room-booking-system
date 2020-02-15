7fcdd073f414e5877f78362f0e1df4838fa60eff
Marcel <marceli.lagoda@gmail.com>
2020-02-15 16:05:55 +0100

    Create database models (Room, Booking)

--- booking_system/models.py
+++ booking_system/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+
+class Room(models.Model):
+    name = models.CharField(max_length=64)
+    capacity = models.IntegerField()
+    has_projector = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Booking(models.Model):
+    date = models.DateTimeField()
+    comment = models.CharField(max_length=170)
+    room = models.ForeignKey(Room, on_delete=models.CASCADE)
+
+    class Meta:
+        unique_together = [['date', 'room']]
