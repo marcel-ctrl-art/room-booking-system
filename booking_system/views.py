34571d2ea1dde6fa1c39d24886be1a0dfa4666e9
Marcel <marceli.lagoda@gmail.com>
2020-02-15 22:18:20 +0100

    Add view for making a reservation

--- booking_system/views.py
+++ booking_system/views.py
@@ -6,4 +6,43 @@ import datetime
 from .forms import CreateRoomForm
 from .models import Room, Booking
 
-# Create your views here.
+
+class MakeReservationView(View):
+    def get(self, request, id):
+        room = get_object_or_404(Room, pk=id)
+        template_name = 'make_reservation.html'
+        ctx = {
+            'room': room
+        }
+
+        return render(request, template_name, ctx)
+
+    def post(self, request, id):
+        room = get_object_or_404(Room, pk=id)
+        template_name = 'make_reservation.html'
+        minimal_date = datetime.datetime.now().date()
+
+        if request.POST.get('book'):
+            booking_date = parser.parse(request.POST.get('booking_date')).date()
+            if booking_date > minimal_date:
+                try:
+                    Booking.objects.get(date=parser.parse(request.POST.get('booking_date')).date())
+                except ObjectDoesNotExist:
+                    Booking.objects.create(
+                        date=parser.parse(request.POST.get('booking_date')).date(),
+                        comment=request.POST.get('comment'),
+                        room=room)
+                    ctx = {
+                        'msg': f"""You've successfully booked a room {room.name} 
+                                    for {parser.parse(request.POST.get('booking_date')).date()}."""
+                    }
+                else:
+                    ctx = {
+                        'msg': "That room has been already booked."
+                    }
+            else:
+                ctx = {
+                    'msg': "Please choose a correct date."
+                }
+
+        return render(request, template_name, ctx)
