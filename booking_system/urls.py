bf0913b91f97c236e98f82f9672edf6af1a2407d
Marcel <marceli.lagoda@gmail.com>
2020-02-17 00:24:35 +0100

    Add url for home view

--- booking_system/urls.py
+++ booking_system/urls.py
@@ -3,6 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('', views.HomeView.as_view(), name='home'),
     path('room/book/<int:id>/', views.MakeReservationView.as_view(), name='booking'),
     path('room/add/', views.CreateRoomView.as_view(), name='add_room'),
     path('room/<int:id>/', views.RoomDetailsView.as_view(), name='room_details'),
