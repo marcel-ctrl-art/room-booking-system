5747c0954456df703304299845cd19760fbf99f8
Marcel <marceli.lagoda@gmail.com>
2020-02-16 21:25:34 +0100

    Update booking_system app urls

--- booking_system/urls.py
+++ booking_system/urls.py
@@ -3,6 +3,11 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('room/<int:id>/', views.MakeReservationView.as_view(), name='booking'),
+    path('room/book/<int:id>/', views.MakeReservationView.as_view(), name='booking'),
     path('room/add/', views.CreateRoomView.as_view(), name='add_room'),
+    path('room/<int:id>/', views.RoomDetailsView.as_view(), name='room_details'),
+    path('room/update/<int:id>/', views.UpdateRoomView.as_view(), name='room_update'),
+    path('room/list/', views.AllRoomsView.as_view(), name='rooms_list'),
+    path('room/delete/<int:id>', views.DeleteRoomView.as_view(), name='delete_room'),
+    path('room/deleted/', views.DeletedRoomView.as_view(), name='deleted_room'),
 ]
